1e4cb12d184f5d8edf9e6d93c85905b6e28d7ff9
Milo <milo.christiansen@gmail.com>
2022-11-21 16:00:55 -0500

    Improve start script.

--- run-everything.sh
+++ run-everything.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-docker network create common
+# Create network if not exists
+docker network inspect common >/dev/null 2>&1 || docker network create common
 
+# Make sure the DB exists so the container can bind it.
 touch feeds.db
-docker run -d \
+
+# Start the app server if it exists (nop if already running), otherwise create it
+docker start rsn-app || docker run -d \
 	-v $PWD/feeds.db:/app/feeds.db \
 	--restart unless-stopped \
 	--network common \
 	--name rsn-app \
 	rsn-app
 
-docker run -d -p 443:443 \
+# And then do the same with NGINX
+docker start nginx || docker run -d -p 443:443 \
 	-v $PWD/NGINX:/etc/nginx/ \
 	-v $PWD/Site:/usr/share/nginx/html \
 	--restart unless-stopped \
