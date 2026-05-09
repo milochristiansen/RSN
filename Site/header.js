de1ebe150734e2b0cff583414d1dc665e5f3485d
Milo <milo.christiansen@gmail.com>
2026-05-09 00:22:25 -0400

    Remove some junk from the common js imports file.

--- Site/header.js
+++ Site/header.js
@@ -8,9 +8,6 @@ import css from "csz"
 // Import PReact
 import { h, render, Component, createRef, createContext } from "preact"
 
-// Enable debugging. Comment this out for production.
-//import "preact/debug"
-
 // Bind PReact to the HTML processor
 const html = htm.bind(h)
 
