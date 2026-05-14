94ecc0703831cdffe5697c16269b79ca27a2413b
Milo <milo.christiansen@gmail.com>
2026-05-14 15:08:57 -0400

    Derp.

--- Site/service-worker.js
+++ Site/service-worker.js
@@ -3,7 +3,6 @@ self.addEventListener('push', (event) => {
     const data = event.data ? event.data.json() : { ts: Date.now(), msg: 'New notification' }
     console.log("Data:", data)
     event.waitUntil(
-        console.log("Sent.")
         self.registration.showNotification('RSN', {
             body: data.msg,
             icon: '/assets/round.svg',
