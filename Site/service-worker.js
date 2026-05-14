34683bc37e7637dab734e4f336317c004de5af82
Milo <milo.christiansen@gmail.com>
2026-05-14 15:04:18 -0400

    Add debug code to the service worker so I can maybe find out why it isn't working on mobile.

--- Site/service-worker.js
+++ Site/service-worker.js
@@ -1,6 +1,9 @@
 self.addEventListener('push', (event) => {
+    console.log("Event: ", event)
     const data = event.data ? event.data.json() : { ts: Date.now(), msg: 'New notification' }
+    console.log("Data:", data)
     event.waitUntil(
+        console.log("Sent.")
         self.registration.showNotification('RSN', {
             body: data.msg,
             icon: '/assets/round.svg',
