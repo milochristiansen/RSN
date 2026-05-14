b4d026efebf2f751c0cdd6804e96501a9892db0f
Milo <milo.christiansen@gmail.com>
2026-05-14 15:20:22 -0400

    Timestamp?!? It doesn't like the timestamp?

--- Site/service-worker.js
+++ Site/service-worker.js
@@ -1,13 +1,10 @@
 self.addEventListener('push', (event) => {
-    console.log("Event: ", event)
     const data = event.data ? event.data.json() : { ts: Date.now(), msg: 'New notification' }
-    console.log("Data:", data)
     event.waitUntil(
         self.registration.showNotification('RSN', {
             body: data.msg,
             icon: '/assets/round.svg',
             badge: '/assets/favicon.svg',
-            timestamp: data.ts,
             requireInteraction: true,
         })
     )
